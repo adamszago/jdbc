a64b0613cee2eac6077118e7bd8b23da3788db0d
Adams Zago <adams.zago@gmail.com>
2019-05-02 23:37:17 -0300

    Projeto

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import com.mysql.jdbc.Connection;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
+	}
+
+}
